fffa13080b7a2ffbf4c6c86147437fa92e8e6295
valeria <valeriairaismeneses@gmail.com>
2017-10-12 10:16:28 -0500

    completado

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector('button').addEventListener('click', function() {
+  var h3 = document.querySelector('h3')
+  var texto = document.querySelector('p')
+  h3.classList.toggle("change")
+  texto.classList.toggle("change")
+
+})
